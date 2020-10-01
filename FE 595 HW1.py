98439ebbe3eef35f591ac9f6411a2c1573ae15d5
ben062930 <70728503+ben062930@users.noreply.github.com>
2020-10-01 13:23:33 -0400

    Update FE 595 HW1.py

--- FE 595 HW1.py
+++ FE 595 HW1.py
@@ -10,6 +10,10 @@ Created on Mon Sep 28 17:15:50 2020
 import numpy as np
 import matplotlib.pyplot as plt
 a = np.arange(0, 2*np.pi, 0.001)
-plt.plot(a, np.cos(a))
-plt.plot(a, np.sin(a))
+plt.plot(a, np.cos(a), color = "green", label = "SIN")
+plt.plot(a, np.sin(a), color = "yellow", label = "COS")
+# plt.show()
+plt.plot(a, np.tan(a), label = "TAN", color = "red")
+plt.ylim(-10,10)
+
 plt.show()
